a7423e13998a76d66d88fd54b03e031c6cf532cf
Toyirjonov <shoxruxmirzo12345@mail.ru>
2025-08-13 18:35:38 +0500

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -83,10 +83,18 @@ function App() {
                                   dispatch(addProductCount(item.id))
                                 }
                               >
-                                <img
-                                  src="../public/images/icon-increment-quantity.svg"
-                                  alt=""
-                                />
+                                <svg
+                                  xmlns="http://www.w3.org/2000/svg"
+                                  width="10"
+                                  height="10"
+                                  fill="none"
+                                  viewBox="0 0 10 10"
+                                >
+                                  <path
+                                    fill="#fff"
+                                    d="M10 4.375H5.625V0h-1.25v4.375H0v1.25h4.375V10h1.25V5.625H10v-1.25Z"
+                                  />
+                                </svg>
                               </button>
                             </div>
                           ) : (
@@ -105,7 +113,7 @@ function App() {
                               }}
                             >
                               <img
-                                src="../public/images/icon-add-to-cart.svg"
+                                src="/images/icon-add-to-cart.svg"
                                 alt="add to cart icon"
                                 width="20"
                                 height="20"
@@ -134,7 +142,7 @@ function App() {
                 <>
                   <img
                     className="menu-right-image"
-                    src="../public/images/illustration-empty-cart.svg"
+                    src="/images/illustration-empty-cart.svg"
                     alt=""
                     width="128"
                     height="128"
@@ -196,14 +204,19 @@ function App() {
 
                   <div className="carbon-neutral">
                     <svg
+                      xmlns="http://www.w3.org/2000/svg"
                       width="21"
                       height="20"
                       fill="none"
-                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 21 20"
                     >
                       <path
-                        d="M8 18.5L2.5 13L3.91 11.59L8 15.67L16.59 7.09L18 8.5L8 18.5Z"
-                        fill="#1ea575"
+                        fill="#1EA575"
+                        d="M8 18.75H6.125V17.5H8V9.729L5.803 8.41l.644-1.072 2.196 1.318a1.256 1.256 0 0 1 .607 1.072V17.5A1.25 1.25 0 0 1 8 18.75Z"
+                      />
+                      <path
+                        fill="#1EA575"
+                        d="M14.25 18.75h-1.875a1.25 1.25 0 0 1-1.25-1.25v-6.875h3.75a2.498 2.498 0 0 0 2.488-2.747 2.594 2.594 0 0 0-2.622-2.253h-.99l-.11-.487C13.283 3.56 11.769 2.5 9.875 2.5a3.762 3.762 0 0 0-3.4 2.179l-.194.417-.54-.072A1.876 1.876 0 0 0 5.5 5a2.5 2.5 0 1 0 0 5v1.25a3.75 3.75 0 0 1 0-7.5h.05a5.019 5.019 0 0 1 4.325-2.5c2.3 0 4.182 1.236 4.845 3.125h.02a3.852 3.852 0 0 1 3.868 3.384 3.75 3.75 0 0 1-3.733 4.116h-2.5V17.5h1.875v1.25Z"
                       />
                     </svg>
                     <p>
